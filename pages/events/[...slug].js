4ca6d62bc50404241d1937148a9d3d9fbcef70ff
Shivam Maurya <mauryashivam050@gmail.com>
2024-10-17 23:49:39 +0530

    page creation

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilterEventPage(){
+    return(
+        <div>
+            <h1>All Event page based on Filter...</h1>
+        </div>
+    )
+}
+
+export default FilterEventPage;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function HomePage(){
+
+  return(
+    <div>
+      <h1>Featured Events</h1>
+    </div>
+  )
+}
+
+export default HomePage;
