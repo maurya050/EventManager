a4206acc168beaa485de8205087654d71fd7d825
Shivam Maurya <mauryashivam050@gmail.com>
2024-10-18 23:30:04 +0530

    event loaded

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,35 @@
+import Link from "next/link";
 function EventItem(props){
+    const {title, image, date, location, id} = props;
 
+    const date_data = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month:'long',
+        year: "numeric"
+    })
+
+    const formattedAddress = location.replace(', ', '\n');
+    const exploredLink = `/events/${id}`;
+
+    return(
+        <li>
+            <img src={"/" + image} alt={title}  style={{height: "200px", width: "200px"}}/>
+            <div>
+                <div>
+                    <h2>{title}</h2>
+                    <div>
+                        <time>{date_data}</time>
+                    </div>
+                    <div>
+                        <address>{}</address>
+                    </div>
+                </div>
+                <div>
+                    <Link href={exploredLink} > Explore Event </Link>
+                </div>
+            </div>
+        </li>
+    )
 }
 
 export default EventItem;
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,13 +1,22 @@
 import EventItem from "./EventItem";
 
-function EventList(props){
+function EventList(props) {
+  const { items } = props;
 
-    const {items}= props;
-
-    return <ul>
-        {
-            items.map(item =>  <EventItem item = {item} />)
-        }
+  return (
+    <ul>
+      {items.map((item) => (
+        <EventItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          date={item.date}
+          location={item.location}
+        />
+      ))}
     </ul>
-
+  );
 }
+
+export default EventList;
