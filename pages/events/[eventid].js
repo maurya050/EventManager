898b5b57266a98210b353679bcbb23aa3c8f9fc1
Shivam Maurya <mauryashivam050@gmail.com>
2024-10-19 10:23:48 +0530

    handled event detail dynamic route

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,9 +1,33 @@
-function EventDetailPage(){
-    return(
-        <div>
-            <h1>Detail Page of an Event</h1>
-        </div>
-    )
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+import { Fragment } from "react";
+
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const events = getEventById(eventId);
+
+  if (!events) {
+    return <p> Some Error !!!</p>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={events.title} />
+      <EventLogistics
+        date={events.date}
+        address={events.location}
+        image={events.image}
+        imageAlt={events.title}
+      />
+      <EventContent>
+        <p>{events.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetailPage;
